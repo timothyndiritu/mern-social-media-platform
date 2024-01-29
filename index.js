26caa1d16f989372a409fbf8f6c52561e786b5e3
Timothy Ndiritu <timmyndiritu@gmail.com>
2024-01-29 12:12:48 +0300

    user routes set-up

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js"
+import { register } from "./controllers/auth.js";
 
 // configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -32,6 +35,15 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   }
 })
+
+// Routes with files
+app.post("/auth/register",upload.single("picture"), register)
+
+
+// Routes
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // Mongoose setup
 
